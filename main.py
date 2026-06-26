e5d21abf6e602bee2b82c581af355a73f064e233
bhatsneha23 <bhatsneha83@gmail.com>
2026-06-26 18:13:28 +0530

    crud done

--- main.py
+++ main.py
@@ -136,4 +136,26 @@ def update_patient(patient_id: str , patient_update = PatientUpdate):
     for key,value in updated_patient_info.items():
         existing_patient_info[key] = value
 
-    existing_patient_info
+    existing_patient_info['id'] = patient_id = patient_id
+    patient_pydantic_obj = Patient(**existing_patient_info)
+
+    existing_patient_info = patient_pydantic_obj.model_dump(exclude ='id')
+
+    data[patient_id]=existing_patient_info
+    save_data(data)
+    return JSONResponse(status_code = 200 , content = {'message': 'Patient updated successfully'})
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id : str):
+
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code = 404 , detail = 'atient not found')
+    
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code = 200 , content = {'message': 'patient deleted'})
+
